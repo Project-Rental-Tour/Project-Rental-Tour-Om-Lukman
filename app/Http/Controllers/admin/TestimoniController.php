bd866ee6de23d2467dd54d4c273ff8f0c0cc82ae
syasul <githubku4@gmail.com>
2025-08-20 07:25:43 +0700

    feat(BE): done implement function crud testimoni

---
+++ app/Http/Controllers/admin/TestimoniController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Testimonial;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class TestimoniController extends Controller
+{
+    public function index()
+    {
+        $currentUser = Auth::user();
+        $testimonials = Testimonial::all();
+
+        if (!$currentUser) {
+            return redirect()->route('login')
+                ->withErrors(['error' => 'You need to login first']);
+        }
+
+        return view('admin.Testimonial', compact('testimonials'));
+    }
+
+    public function store(Request $request)
+    {
+        $validate = $request->validate([
+            'name' => 'required|string|max:255',
+            'message' => 'required|string|max:255',
+        ]);
+
+        Testimonial::create($validate);
+
+        return redirect()->route('testimoni.index')->with('success', 'done send testimoni');
+    }
+}
