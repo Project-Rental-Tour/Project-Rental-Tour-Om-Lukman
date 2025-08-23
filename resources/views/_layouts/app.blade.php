9bb0c47e64664815e5783ad45d0ff64985e31eb0
syasul <githubku4@gmail.com>
2025-08-23 07:05:27 +0700

    feat(FE): change title name

--- resources/views/_layouts/app.blade.php
+++ resources/views/_layouts/app.blade.php
@@ -5,7 +5,7 @@
     <!-- Meta tags dan CSS -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>@yield('title', 'WK')</title>
+    <title>@yield('title', 'SEPTEM TOUR')</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     @yield('head')
--- resources/views/_layouts/user.blade.php
+++ resources/views/_layouts/user.blade.php
@@ -4,10 +4,11 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>@yield('title', 'WK')</title>
+    <title>@yield('title', 'SEPTEM TOUR')</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     @yield('head')
+    @stack('styles')
 </head>
 
 <body>
