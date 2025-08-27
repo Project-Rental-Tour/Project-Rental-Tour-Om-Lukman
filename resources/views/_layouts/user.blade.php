29c5d4487b42eca6452bfac83f5158b7155dd08f
syasul <githubku4@gmail.com>
2025-08-27 10:49:24 +0700

    feat(FE): add image and implement favicon

--- resources/views/_layouts/user.blade.php
+++ resources/views/_layouts/user.blade.php
@@ -3,6 +3,7 @@
 
 <head>
     <meta charset="utf-8" />
+    <link rel="icon" type="image/x-icon" href="{{ asset('assets/images/favicon.ico') }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>@yield('title', 'SEPTEM TOUR')</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
