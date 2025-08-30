1b29daa1cfacde45b01d6556b81d773867cef93e
syasul <githubku4@gmail.com>
2025-08-31 05:06:00 +0700

    feat(BE): change pallete file FE

--- app/Http/Controllers/auth/AuthController.php
+++ app/Http/Controllers/auth/AuthController.php
@@ -14,7 +14,7 @@ class AuthController extends Controller
 {
     public function showLoginForm()
     {
-        return view('auth.loginAdmin');
+        return view('Auth.LoginAdmin');
     }
 
     public function login(Request $request)
