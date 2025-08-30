c9a25656d8856143e5679426275d17d6a554b23c
syasul <githubku4@gmail.com>
2025-08-31 04:38:32 +0700

    feat(BE): change pallete file FE

--- app/Http/Controllers/admin/BookingController.php
+++ app/Http/Controllers/admin/BookingController.php
@@ -85,7 +85,7 @@ class BookingController extends Controller
         $destination = Destination::where('slug', $slug)->firstOrFail();
         $profiles = Profile::find(1);
 
-        return view('client.RegularBooking', compact('destination', 'profiles'));
+        return view('client.regularBooking', compact('destination', 'profiles'));
     }
 
     public function bookingRegular(Request $request)
@@ -160,7 +160,7 @@ class BookingController extends Controller
     {
         $profiles = Profile::find(1);
 
-        return view('client.CustomBooking', compact('profiles'));
+        return view('client.customBooking', compact('profiles'));
     }
 
     public function bookingCustom(Request $request)
