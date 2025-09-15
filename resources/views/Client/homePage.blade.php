a55eef7a63cc7761cc4b1387bb302435885b5b7b
syasul <githubku4@gmail.com>
2025-09-15 17:36:44 +0700

    feat(FE): revison

--- resources/views/Client/homePage.blade.php
+++ resources/views/Client/homePage.blade.php
@@ -47,7 +47,7 @@
                 </p>
             </div>
 
-            <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+            <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
                 @foreach([
                     ['Expert Local Guides', 'We partner with knowledgeable local guides who bring destinations to life with authentic stories and insights.', 'M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z', 'blue'],
                     ['Competitive Pricing', 'Affordable prices with best value packages for all travelers.', 'M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z', 'yellow'],
