87124baa08c481260c2d735bed53b0552f8ea5d0
syasul <githubku4@gmail.com>
2025-09-17 19:57:07 +0700

    feat(FE): fix overflow

--- resources/views/components/admin/modal/modal-destination/edit-destination.blade.php
+++ resources/views/components/admin/modal/modal-destination/edit-destination.blade.php
@@ -193,7 +193,7 @@
                         <trix-editor 
                             input="note"
                             class="border border-gray-300 rounded-lg bg-white"
-                            style="max-height: 200px;"
+                            style="max-height: 200px; overflow-y: auto;"
                         ></trix-editor>
                         <p class="text-xs text-gray-400 mt-1">Use this to provide extra information for customers.</p>
                     </div>
@@ -207,7 +207,7 @@
                         <trix-editor 
                             input="itinerary"
                             class="border border-gray-300 rounded-lg bg-white"
-                            style="max-height: 200px;"
+                            style="max-height: 200px; overflow-y: auto;"
                         ></trix-editor>
                     </div>
                     <div>
