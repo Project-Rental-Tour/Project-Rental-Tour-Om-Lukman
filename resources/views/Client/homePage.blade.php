e2fd346b8c898e8d25ca97a6fb72d08d62ffb5a9
syasul <githubku4@gmail.com>
2025-12-17 09:17:35 +0700

    .

--- resources/views/Client/homePage.blade.php
+++ resources/views/Client/homePage.blade.php
@@ -465,7 +465,7 @@
                                     <img 
                                         src="{{ asset('storage/' . $testimoni->image) }}" 
                                         alt="{{ $testimoni->name }}"
-                                        class="w-full h-fit object-cover rounded-2xl mb-6"
+                                        class="w-full h-fit object-cover rounded-2xl"
                                     >
 
                                 {{-- <div class="text-secondary mb-4 text-sm">
