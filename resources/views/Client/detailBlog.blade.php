efe5adea2b7d3d7f561f1bc9ac90f357fbfe52af
syasul <githubku4@gmail.com>
2025-12-30 19:53:53 +0700

    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/js/all.min.js"></script>

--- resources/views/Client/detailBlog.blade.php
+++ resources/views/Client/detailBlog.blade.php
@@ -6,7 +6,8 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     {{-- UNIFIED FONT TO POPPINS ONLY --}}
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;1,400&display=swap" rel="stylesheet">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/js/all.min.js"></script>
+    <!--  <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/js/all.min.js"></script> -->
+    <script src="{{ asset('assets/js/all.min.js') }}"></script>
 
     <style>
         /* --- Premium Blue Ocean Theme Configuration --- */
